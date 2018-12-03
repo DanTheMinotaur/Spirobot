747d42fb569e477c6fdb026cd64f9fe884bfb2c8
DanTheMinotaur <x15004091@student.ncirl.ie>
2018-12-03 18:24:17 +0000

    Added Config method and CLI Interface

--- bot/parts/body.py
+++ bot/parts/body.py
@@ -1,4 +1,10 @@
 from bot.parts.leg import Leg
+import json
 
 class Body:
-    pass
+    @staticmethod
+    def load_config(json_file='config.json'):
+        config_data = None
+        with open(json_file) as f:
+            config_data = json.load(f)
+        return config_data
