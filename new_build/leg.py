e6d2f933ddd6ea42bd9976d0969b0ca2ee68529d
daniel <x15004091@student.ncirl.ie>
2019-01-29 20:09:44 +0000

    New Brand for code rewrite.

--- new_build/leg.py
+++ new_build/leg.py
@@ -9,8 +9,7 @@ class Leg:
         try:
             self.upper = positions["upper"]
             self.middle = positions["middle"]
-            self.lower = positions["lower"]
-            self.name = positions["position"]
+            self.name = positions["position"].lower()
             self.motors = (
                 self.upper,
                 self.middle,
@@ -29,11 +28,11 @@ class Leg:
 
     def move_leg_forward(self):
         if "left" in self.name.lower():
-            self.kit.servo[self.middle].angle = 170
-            self.kit.servo[self.upper].angle = 10
+            self.kit.servo[self.middle].angle = 160
+            self.kit.servo[self.upper].angle = 20
         else:
-            self.kit.servo[self.middle].angle = 10
-            self.kit.servo[self.upper].angle = 170
+            self.kit.servo[self.middle].angle = 20
+            self.kit.servo[self.upper].angle = 160
         sleep(.5)
         self.kit.servo[self.middle].angle = 90
         sleep(.5)
@@ -43,27 +42,29 @@ class Leg:
             self.kit.servo[motor].angle = 90
 
 
-config_left = {
-    "position": "FRONTLEFT",
-    "upper": 0,
-    "middle": 1,
-    "lower": 2
-}
+legs = []
 
-config_right = {
-    "position": "FRONTRIGHT",
-    "upper": 3,
-    "middle": 4,
-    "lower": 5
-}
+config = [
+    {
+        "name": "frontright",
+        "upper": 14,
+        "middle": 15
+    }
+]
 
-test_left = Leg(config_left)
+for c in config:
+    legs.append(Leg(c))
 
-test_right = Leg(config_right)
+for l in legs:
+    if "back" in l.name or "front" in l.name:
+        l.set_initial_position()
+        l.move_leg_forward()
 
+for l in legs:
+    if "back" in l.name or "front" in l.name:
+        l.set_initial_position()
 
-test_right.move_leg_forward()
-test_left.move_leg_forward()
-test_right.set_initial_position()
-test_left.set_initial_position()
-
+"""
+FRM = 15
+FRU = 14
+"""
