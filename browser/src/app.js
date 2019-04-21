e4f86727626419e44acc1342bacaac7da7efc763
Daniel <x15004091@student.ncirl.ir>
2019-04-21 19:53:42 +0100

    Implemented Joystick with bad styling

--- browser/src/app.js
+++ browser/src/app.js
@@ -1,9 +1,11 @@
 // Import Styles
 require( './scss/style.scss');
+
 import 'bulma/bulma.sass'
 
-const loginTemplate = require('./js/login.html.handlebars');
-const adminTemplate = require('./js/control-panel.html.handlebars');
+
+const loginTemplate = require('./templates/login.html.handlebars');
+const adminTemplate = require('./templates/control-panel.html.handlebars');
 
 // Wait for main content to load
 document.addEventListener("DOMContentLoaded", event => {
@@ -18,6 +20,8 @@ document.addEventListener("DOMContentLoaded", event => {
                 appJS.innerHTML = adminTemplate(user);
 
                 const bot = new BotController(app);
+                const joystick = createJoystick(document.getElementById("joystick"));
+                setInterval(() => console.log(joystick.getPosition()), 16);
 
                 bot.move("forward");
 
@@ -76,4 +80,66 @@ function loginGoogle() {
     }).catch(e => {
         console.log(e); // Log Error
     });
+}
+
+function createJoystick(parent) {
+    const maxDiff = 100;
+    const stick = document.createElement('div');
+    stick.classList.add('joystick');
+
+    stick.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+    stick.addEventListener('touchstart', handleMouseDown);
+    document.addEventListener('touchmove', handleMouseMove);
+    document.addEventListener('touchend', handleMouseUp);
+
+    let dragStart = null;
+    let currentPos = { x: 0, y: 0 };
+
+    function handleMouseDown(event) {
+        stick.style.transition = '0s';
+        if (event.changedTouches) {
+            dragStart = {
+                x: event.changedTouches[0].clientX,
+                y: event.changedTouches[0].clientY,
+            };
+            return;
+        }
+        dragStart = {
+            x: event.clientX,
+            y: event.clientY,
+        };
+
+    }
+
+    function handleMouseMove(event) {
+        if (dragStart === null) return;
+        event.preventDefault();
+        if (event.changedTouches) {
+            event.clientX = event.changedTouches[0].clientX;
+            event.clientY = event.changedTouches[0].clientY;
+        }
+        const xDiff = event.clientX - dragStart.x;
+        const yDiff = event.clientY - dragStart.y;
+        const angle = Math.atan2(yDiff, xDiff);
+        const distance = Math.min(maxDiff, Math.hypot(xDiff, yDiff));
+        const xNew = distance * Math.cos(angle);
+        const yNew = distance * Math.sin(angle);
+        stick.style.transform = `translate3d(${xNew}px, ${yNew}px, 0px)`;
+        currentPos = { x: xNew, y: yNew };
+    }
+
+    function handleMouseUp(event) {
+        if (dragStart === null) return;
+        stick.style.transition = '.2s';
+        stick.style.transform = `translate3d(0px, 0px, 0px)`;
+        dragStart = null;
+        currentPos = { x: 0, y: 0 };
+    }
+
+    parent.appendChild(stick);
+    return {
+        getPosition: () => currentPos,
+    };
 }
