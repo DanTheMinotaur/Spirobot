501753d28152161bd9d75361bac97d7ae03fe9ff
Daniel <x15004091@student.ncirl.ir>
2019-05-12 00:03:47 +0100

    Moved Login Functions to own file

--- browser/src/app.js
+++ browser/src/app.js
@@ -3,6 +3,7 @@ require( './scss/style.scss');
 
 import 'bulma/bulma.sass';
 import {UIController} from "./js/ui_controller";
+import {logoutListener, addLoginListeners} from "./js/login_controllers";
 
 const loginTemplate = require('./templates/login.html.handlebars');
 const adminTemplate = require('./templates/control-panel.html.handlebars');
@@ -35,75 +36,3 @@ document.addEventListener("DOMContentLoaded", event => {
         console.error(e);
     }
 });
-
-/**
- * Add Listener events for login screen
- */
-function addLoginListeners() {
-    let googleLogin = document.getElementById("googleLogin");
-    googleLogin.addEventListener("click", (event) => {
-        event.preventDefault();
-        loginGoogle();
-    });
-    let emailLogin = document.getElementById("emailLogin");
-    emailLogin.addEventListener("click", (event) => {
-       event.preventDefault();
-       let email = document.getElementById("emailInput");
-       let password = document.getElementById("passwordInput");
-       loginEmail(email.value, password.value);
-    });
-}
-
-/**
- * Loads Login Popup window
- */
-function loginGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase.auth().signInWithPopup(provider).then(loginResult => {
-        const user = loginResult.user;  // User Auth Successful
-        console.log(user);
-    }).catch((error) => {
-        console.log("Could Not Login: ", error); // Log Error
-        displayLoginError(error);
-    });
-}
-
-function loginEmail(email, password) {
-    firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
-        console.log("Could Not Login: ", error);
-        displayLoginError(error);
-    });
-}
-
-function displayLoginError(error) {
-
-    let loginErrorDisplay = document.getElementById("loginError");
-        document.getElementById("closeLoginError").addEventListener("click", () => {
-            loginErrorDisplay.style.display = "none";
-    });
-    let header = loginErrorDisplay.getElementsByTagName("p")[0];
-    let body = loginErrorDisplay.getElementsByClassName("message-body")[0];
-    if (error.code === "auth/user-not-found") {
-        header.innerHTML = "No Account Found for User";
-    } else if (error.code === "auth/wrong-password") {
-        header.innerHTML = "Wrong Password";
-    } else {
-        header.innerHTML = "Login Error";
-    }
-    body.innerHTML = error.message;
-    loginErrorDisplay.style.display = "block";
-}
-
-/**
- * Function for adding logout events to browser if a user is logged in.
- * @param app Firebase app object
- * @param firebaseAuth Firebase Auth Object.
- */
-function logoutListener(app, firebaseAuth) {
-    document.getElementById("logout").addEventListener("click", () => {
-        firebaseAuth.signOut().then(() => {
-            app.innerHTML = loginTemplate({});
-        });
-    })
-}
